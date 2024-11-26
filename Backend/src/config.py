3cee5b8badcf47bcf2ab8109a166de51b4add086
Xowmandon <71954990+Xowmandon@users.noreply.github.com>
2024-11-26 08:10:25 -0800

    Added Config Settings for Connecting Flask to RDS Instance and Local DB Config

---
+++ Backend/src/config.py
@@ -0,0 +1,31 @@
+
+
+from sqlalchemy.engine.url import URL
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = URL.create(
+        "postgresql",
+        password="Capstone2024!?",  # plain (unescaped) text
+        host="captstone-db.cvkkwcai4hfp.us-west-1.rds.amazonaws.com",
+        port=5432,
+        database="Unhinged_DB",
+    )
+    
+    # Track modifications of objects and emit signals
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # Echo SQL Queries into the Console
+    SQLALCHEMY_ECHO = False
+    
+# TODO - Refactor/Test Config for Test Environment
+class ConfigTestLocal:
+    SQLALCHEMY_DATABASE_URI = URL.create(
+        "postgresql",
+        password="Capstone2024!?",  # plain (unescaped) text
+        host="localhost",
+        port=5432,
+        database="Unhinged_DB_Test",
+    )
+    
+    # Echo SQL Queries into the Console
+    SQLALCHEMY_ECHO = True
