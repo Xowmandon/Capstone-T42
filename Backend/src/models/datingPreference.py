2a3857223bb381759d90a6b7ef59fa5790110351
Xowmandon <71954990+Xowmandon@users.noreply.github.com>
2024-12-11 02:16:59 -0800

    Add ONDELETE CASCADE to datingPreferences table - Model is not yet finished or implemented

--- Backend/src/models/datingPreference.py
+++ Backend/src/models/datingPreference.py
@@ -23,7 +23,7 @@ class DatingPreference(db.Model):
     
     # Reference to Users Table
     # On Delete Cascade to Remove Dating Preference if User is Deleted
-    user = db.Column(db.Integer, db.ForeignKey('users.id'), unique=True,nullable=False)
+    user = db.Column(db.Integer, db.ForeignKey('users.id',ondelete="CASCADE"), unique=True,nullable=False)
     
     
     # String representation of a Dating Preference, Outputting each Field Associated
