02386dd153b350ec4c94b637863227ecb785fcf9
Xowmandon <josh.ferguson1016@gmail.com>
2025-02-18 03:59:49 -0800

    Extended Bcrypt and JWT For use in Flask

--- Backend/src/extensions.py
+++ Backend/src/extensions.py
@@ -2,10 +2,14 @@ from flask import app
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_socketio import SocketIO
-
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 ma = Marshmallow(app)
+
+jwt = JWTManager()
+bcrypt = Bcrypt()
 
 
 # Initialize SocketIO TODO
@@ -14,14 +18,6 @@ ma = Marshmallow(app)
 URL = "https://cowbird-expert-exactly.ngrok-free.app"
 
 
-# TODO - TODO: Consider Using FlaskMarshmallow for Serialization/Deserialization
-# - Can also Include Validation and Error Handling Easier than Manual Methods like to_dict
 
-#----If Flask-Marshmallow is  Not Used, Add the Following Code----
-# TODO: ADD def to_dict for Each Model to Serialize to JSON
-# TODO: ADD def from_dict for Each Model to Deserialize from JSON
-
-# TODO: ADD def to_csv for Each Model to Serialize to CSV
-# TODO: ADD def from_csv for Each Model to Deserialize from CSV
 
 #------------------------------------------------------------
