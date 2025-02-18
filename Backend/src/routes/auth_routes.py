24e9028473a7819e4a81dee97117521f5e17bab5
Xowmandon <71954990+Xowmandon@users.noreply.github.com>
2025-02-18 11:28:42 -0800

    Cleanup of Import Statements

--- Backend/src/routes/auth_routes.py
+++ Backend/src/routes/auth_routes.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify, Blueprint
 from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, JWTManager
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 from Backend.src.extensions import db # Import the DB Instance
 import Backend.src.models as models # Import the Models and Schemas
