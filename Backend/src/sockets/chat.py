a14e3a3158053558a40c7bdba79697cd6bd26caf
Xowmandon <71954990+Xowmandon@users.noreply.github.com>
2025-02-28 05:56:43 -0800

    Fleshed out Chat and Swipe Websocket Namespaces/Rooms. Swipe can reasonably detect and emit when a Match is Successful, and Chat can allocate and emit to proper rooms on Client-Side.
    - Chat is functional, swipe namespace needs some refinement

--- Backend/src/sockets/chat.py
+++ Backend/src/sockets/chat.py
@@ -1,63 +1,199 @@
-from flask import Flask, request
-from flask_socketio import SocketIO, Namespace, emit, join_room, leave_room
-from flask_jwt_extended import JWTManager, decode_token
+"""
+Author: Joshua Ferguson
+
+Implementation of the `ChatNamespace` class, which handles chat socket events for real-time messaging.
+
+The `ChatNamespace` class provides the following methods:
+
+# Main WebSocket Event Handlers
+
+- `on_connect()`: Authenticates the user via JWT when they connect and auto-joins match rooms.
+- `on_join(data)`: User joins a chat room (f"match_<match_id>" used as room identifier).
+- `on_message(data)`: Handles message sending and Saving to DB
+- `on_leave(data)`: User leaves a chat room.
+- `on_disconnect()`: Handles user disconnection and removes them from any match rooms.
+
+# Custom Methods and Helpers
+
+- `get_user_match_rooms()`: Retrieves all match rooms where this user is involved.
+- `auto_join_match_rooms()`: Retrieves and joins all match rooms where this user is not joined.
+- `auto_leave_match_rooms()`: Leaves all match rooms where this user is joined.
+
+"""  
+import json
+from flask import  request
+from flask_socketio import Namespace, emit, join_room, leave_room, disconnect, rooms
+
+from Backend.src.models.model_helpers import MatchModelHelper
+import Backend.src.models as models
+from Backend.src.services.auth_service import get_user_from_token
+from Backend.src.sockets.socket_helpers import auth_connecting_user, gen_match_room_name
 from Backend.src.extensions import redis_client
-from Backend.app import socketio
+
 
 class ChatNamespace(Namespace):
     """Handles chat socket events for real-time messaging."""
 
-    def on_connect(self):
-        """Authenticate user via JWT when they connect."""
-        self.user_token = request.args.get("token")
-        if not self.user_token:
-            print("Missing token. Connection refused.")
-            return False  # Reject connection
+    # --------------------Standard SocketIO Methods ----------------------------------------------
 
-        try:
-            decoded_token = decode_token(self.user_tokentoken)
-            self.user_id = decoded_token.get("sub")
-            print(f"User {self.user_token} connected to chat namespace!")
-        except:
-            print("Invalid JWT token. Connection refused.")
-            return False
+    def on_connect(self):
+        """Authenticate user via JWT when they connect and auto-join match rooms."""
+        
+        auth_header = request.headers.get("Authorization")
+        user_id = auth_connecting_user(auth_header)
+        if user_id is None:
+            disconnect()
+            return
+        
+        self.user_id = user_id
+        
+        # Store user_id for easy access
+        print(f"User {self.user_id} connected to /Chat namespace!")
+
+        # Auto-Join all match rooms of the user
+        self.auto_join_match_rooms()
 
     def on_join(self, data):
         """User joins a chat room (match_id used as room identifier)."""
-        match_id = data["match_id"]
-        join_room(match_id,sid=self.user_token)
-        print(f"User_id {self.user_id} joined Match-Chat {match_id}")
+        match_id = data.get("match_id")
+        match_room= gen_match_room_name(match_id)
+        
+        if not match_id:
+            print("Missing room_id/match_id. Join request ignored.")
+            return
 
-        emit("status", {"msg": f"User {self.user_token} joined"}, room=match_id)
+        # Join the chat room for match_id
+        join_room(match_room)
+        print(f"User {self.user_id} joined Match-Chat {match_room}")
+        
+        # Emit status message to all users in the chat room that the user has joined
+        emit("status", {"msg": f"User {self.user_id} joined"}, room=match_room)
+        
+        
+        # Get 10 Most Recent Messages from Postgres 
+        MatchHelper = MatchModelHelper(match_id=match_id)
+        recent_messages = MatchHelper.get_messages(limit=10, page=1, get_all_messages=False)
+        
+        # Emit Recent Messages to User
+        emit("chat_history", {"match_id": match_id, "messages": recent_messages}, room=self.user_id)
+        
+        # TODO - Implement Caching/Queueing Messages in Redis
+        # Retrieve last 10 messages from Redis/PostGres for chat history
+        #chat_history = redis_client.lrange(str(match_id), -10, -1)
+        #chat_messages = [json.loads(msg) for msg in chat_history] if chat_history else []
+        #emit("chat_history", {"match_id": match_id, "messages": chat_messages}, room=self.user_id)
 
     def on_message(self, data):
         """Handle message sending."""
-        match_id = data["match_id"]
-        message = data["message"]
-    
-        print(f"User_id {self.user_id} sent message '{message} in Match-Chat  {match_id}")
-        
-        # Save message temporarily in Redis
-        redis_client.rpush(match_id, f"{self.user_id}:{message}")
+        match_id = data.get("match_id")
+        match_room = gen_match_room_name(match_id)
+        message_content = data.get("message_content")
 
-        # Broadcast message to all users in the chat room
-        emit("message", 
-             {"messager": self.user_token, "message": message}, 
-             room=match_id
-             )
+        if not match_id or not message_content:
+            print("Invalid message data. Ignored.")
+            return
+
+        print(f"User {self.user_id} sent message '{message_content}' in Match-Chat {match_room}")
+
+        # Save message temporarily in Redis
+        #message_data = json.dumps({"messager": self.user_id, "message": message})
+        #redis_client.rpush(str(match_id), message_data)
+
+        # Save message to Postgres - Persisting the Message
+        MatchHelper = MatchModelHelper(match_id=match_id)
+        saved_message = MatchHelper.save_message_to_match_chat(self.user_id, message_content=message_content)
+
+        if not saved_message:
+            print(f"Failed to save message to DB: {json.dumps(saved_message)}")
+            return
+    
+        # Broadcast message to all users in the Match chat room, CLient should Update UI 
+        emit("message", {"sender": self.user_id, "message": message_content}, room=match_room)
+        
+        # TODO - Broadcast Message to Notification Namespace for Push Notification Service
 
     def on_leave(self, data):
         """User leaves a chat room."""
-        match_id = data["match_id"]
-        leave_room(match_id)
-        print(f"User {self.user_id} left room {match_id}")
+        match_id = data.get("match_id")
+        match_room = gen_match_room_name(match_id)
+        if not match_id:
+            print("Missing match_id. Leave request ignored.")
+            return
 
-        emit("status", {"msg": f"User {self.user_id} left"}, room=match_id)
+        leave_room(match_room)
+        print(f"User {self.user_id} left Match-Chat {match_room}")
+
+        emit("status", {"msg": f"User {self.user_id} left"}, room=match_room)
 
     def on_disconnect(self):
-        """Handle user disconnection."""
-        print(f"User {self.user_id} disconnected")
+        """Handle user disconnection and remove them from any match rooms."""
+        
+        if self.user_id is None:
+            print("User is not authenticated. Disconnect event ignored.")
+            return
+    
+        user = models.User.query.get(self.user_id)
+        
+        # Retrieve all match IDs where the user is involved
+        match_ids = user.get_match_ids()
+        
+        # Generate room IDs for each match - Standardize Room Names "match_<match_id>"
+        match_room_ids = [gen_match_room_name(match_id) for match_id in match_ids] # room_id is the match_id
+        
+        # Join each match room
+        for match_room in match_room_ids:
+            # Join the room if the user is not already in it
+            if match_room  in rooms(self.user_id):
+                leave_room(match_room, sid=self.user_id)
+                print(f"User {self.user_id} left room: {match_room}")
 
+        # Emit Disconnect Status Message to User
+        emit("status", {"msg": f"User {self.user_id} disconnected"}, room=self.user_id)
+        print(f"User {self.user_id} disconnected from /Chat namespace!")
+        
+        
+    # --------------------Custom Methods ----------------------------------------------
+        
+    def get_user_match_rooms(self):
+        """Retrieve all match rooms where this user is involved."""
+        user = models.User.query.get(self.user_id)
+        # Retrieve all match IDs where the user is involved
+        match_ids = user.get_match_ids()
+        # Generate room IDs for each match - Standardize Room Names "match_<match_id>"
+        match_room_ids = [gen_match_room_name(match_id) for match_id in match_ids] # room_id is the match_id
+        return match_room_ids
+    
+    def auto_join_match_rooms(self):
+        """Retrieve and join all match rooms where this user is Not Joined."""
+        match_room_ids = self.get_user_match_rooms()
+       
+        # Join each match room
+        for room_id in match_room_ids:
+            
+            # Join the room if the user is not already in it
+            if room_id not in rooms(self.user_id):
+                join_room(room_id)
+                print(f"User {self.user_id} auto-joined Match-Chat {room_id}")
 
-# Register the chat namespace
-socketio.on_namespace(ChatNamespace("/chat"))
+            else:
+                print(f"User {self.user_id} is already in Match-Chat {room_id}")
+
+        emit("status", {"msg": f"User {self.user_id} auto-joined Match-Chats on Connection"}, room=self.user_id)
+
+    def auto_leave_match_rooms(self):
+        """Leave all match rooms where this user is Joined."""
+        
+        match_room_ids = self.get_user_match_rooms()
+        
+        # Leave each match room
+        for room_id in match_room_ids:
+            if room_id in rooms(self.user_id):
+                leave_room(room_id, sid=self.user_id)
+                print(f"User {self.user_id} auto-left Match-Chat {room_id}")
+            else:
+                print(f"User {self.user_id} is not in Match-Chat {room_id}")
+                
+        emit("status", {"msg": f"User {self.user_id} auto-left Match-Chats on Disconnection"}, room=self.user_id)    
+            
+# Register the chat namespace (Currently in App.py)
+#socketio.on_namespace(ChatNamespace("/chat"))
--- Backend/src/sockets/swiping.py
+++ Backend/src/sockets/swiping.py
@@ -1,93 +1,111 @@
 from flask import Flask, request
-from flask_socketio import SocketIO, Namespace, emit, join_room, leave_room
+from flask_socketio import SocketIO, Namespace, disconnect, emit, join_room, leave_room
 from flask_jwt_extended import JWTManager, decode_token
 import redis
 
-from Backend.app import socketio
 from Backend.src.extensions import db
-from Backend.src.models import Swipe, Match
-
-
+import  Backend.src.models as models
+from Backend.src.services.auth_service import get_user_from_token
 # Listen for Swipe Events, Emits Back Successful Match Message if Swipe is accepted
+
 class SwipeNamespace(Namespace):
+    def __init__(self, namespace=None):
+        super().__init__(namespace)
+        self.user_id = None  # Initialize user_id as None
+
+
+    # -----------------Main Websocket Events---------------------
+
     def on_connect(self):
         """Authenticate user via JWT when they connect."""
-        self.user_token = request.args.get("token")
-        if not self.user_token:
-            print("Missing token. Connection refused.")
-            return False  # Reject connection
 
-        try:
-            decoded_token = decode_token(self.user_tokentoken)
-            self.user_id = decoded_token.get("sub")
-            print(f"User {self.user_token} connected to Swipe namespace!")
-        except:
-            print("Invalid JWT token. Connection refused.")
+        user_token = request.args.get("token")
+        
+        if not user_token:
+            print("Missing token. Connection refused.")
+            disconnect()
             return False
 
-    def on_disconnect(self):
-        print('Client disconnected')
+        user = get_user_from_token(user_token)
+        
+        if user is None:
+            print("Invalid JWT token or User Does not Exist. Connection refused.")
+            disconnect()
+            return False
+
+        self.user_id = user.id
+        
+        print(f"User {self.user_id} connected to Swipe namespace!")
 
     def on_swipe(self, data):
+        """Process swipe event."""
+        if self.user_id is None:  # Cleaner authentication check
+            print("User is not authenticated. Swipe event ignored.")
+            return
+        
+        swipee_id = data.get("swipee_id")
+        new_swipe_result = data.get("swipe_result")
+
+        if not swipee_id or not new_swipe_result:
+            print("Invalid swipe data received.")
+            return
+        
+        print(f"User {self.user_id} swiped '{new_swipe_result}' on User {swipee_id}")
+
+        # Process swipe logic
+        processed_swipe = models.swipe.SwipeProcessor.process_new_swipe(self.user_id, swipee_id, new_swipe_result)
+
+        # Process New Match if Swipe is Accepted - Create Match and Emit to Both Users
+        if processed_swipe and processed_swipe.swipe_result == "ACCEPTED":
+            print(f"New Match Detected: {processed_swipe}")
+            self.process_new_match(swipee_id)
+
+    def on_disconnect(self):
+        """Disconnect event."""
+        
+        if self.user_id is None: #Handle Disconnection of Unauthenticated Users without Attrubute Error
+            print("Invalid user_id User disconnected from Swipe namespace - Ignored.")
+        else:
+            print(f"User {self.user_id} disconnected from Swipe namespace.")
+            
+        # Emit Disconnect Status Message to User
+        emit("status", {"msg": f"User {self.user_id} disconnected"}, room=self.user_id)
+        return
+        #self.user_id = None
+
+    # -----------------Custom/Helper Methods & Events---------------------
+
+    def process_new_match(self, matchee_id):
         """
-        Process the swipe event and check if the swipe is accepted.
+        Process the New Match Event and Create a New Match
 
         Args:
-            data (dict): A dictionary containing the swipe data, including the swipee_id and swipe_result.
+            matchee_id (int): The ID of the user being matched.
 
         Returns:
-            None if not a Successful Match
-            
-            If Successful Match Detected, Returns Success Msg and swipee_id and Created match_id
+            - None if not a Successful Match
+            - If Successful Match Detected, Emits success message to both users.
         """
-        # Get Current User (self.user_token)
-        # Get User that Current User Swiped On 
-        swipee_id = data["swipee_id"]
-        
-        # Get Swipe Result
-        new_swipe_result = data["swipe_result"]
-        
-        print(f"User {self.user_id} Swiped '{new_swipe_result} on User {swipee_id}")
-        
-        # Check if Swipe Exists for the two Users
-        # Meaning, There is a record Where Swipee equals Current Swiper, and Vice Versa
-        swipe_exists = Swipe.query.filter_by(swiper_id=swipee_id, swipee_id=self.user_id).first()
-        
-        # If swipe does not exist, create a new swipe instance
-        if not swipe_exists:
-            
-            new_swipe = Swipe(swiper_id=self.user_id, swipee_id=swipee_id, swipe_result=new_swipe_result)
-            db.session.add(new_swipe)
-            db.session.commit()
-            print(f"New swipe created - {Swipe.swipee_id}, {Swipe.swiper_id}, {Swipe.swipe_result}")
-            pass
-        
-        # Swipe Exits - Good to Compare and Check for New Match
-        
-        # If Already Rejected, Pass
-        elif swipe_exists.swipe_result == "REJECTED":
-            pass
-        
-        # If New Swipe is Rejected, set Stored Swipe Record as Rejected, Pass
-        elif new_swipe_result == "REJECTED":
-            swipe_exists.swipe_result = "REJECTED"
-            pass
-
-        # If Both Stored Swipe and New is PENDING, Turn to Accepted, Indicating Successful Match
-        elif swipe_exists.swipe_result == "PENDING" and new_swipe_result == "PENDING":
-            swipe_exists.swipe_result == "ACCEPTED"
-        
-            # Create New Match, Push to DB, and Emit MSG Back to Client
-            print(f"New Match Detected from Swipe - {Swipe.swipee_id}, {Swipe.swiper_id}, {Swipe.swipe_result}")
-            
-            new_match = Match().create_match(matcher_id=self.user_id,matchee_id=swipee_id)
+        try:
+            # Create New Match and Push to DB
+            new_match = models.match.Match().create_match(matcher_id=self.user_id, matchee_id=matchee_id)
             
             if new_match:
-                # Emit Successful Match to Client, with Swipee ID and Match ID
-                emit('successful_match', {'swipee_id': swipee_id, 'match_id': new_match.id})
-            else:
-                # Emit Error Code, Tried to Create Match, Failed
-                emit('match_creation_failed', {'Current User -> swipee_id': swipee_id})
+                print(f"Match Created: {new_match}")
 
-# Register the chat namespace
-socketio.on_namespace(SwipeNamespace("/swipe"))
+                # Notify both users about the successful match
+                emit('successful_match', {'matchee_id': matchee_id, 'match_id': new_match.id}, room=self.user_id)
+                emit('successful_match', {'matchee_id': self.user_id, 'match_id': new_match.id}, room=matchee_id)
+                
+                 # TODO - Broadcast Message to Notification Namespace for Push Notification Service
+            else:
+                print(f"Failed to create match between {self.user_id} and {matchee_id}")
+                emit('match_creation_failed', {'matchee_id': matchee_id, 'message': 'Match creation failed'}, room=self.user_id)
+
+        except Exception as e:
+            print(f"Error in processing match: {str(e)}")
+            emit('match_creation_failed', {'matchee_id': matchee_id, 'message': 'Internal server error'}, room=self.user_id)
+
+# Register the chat namespace (CURRENTLY IN APP)
+#socketio.on_namespace(SwipeNamespace("/swipe"))
+
