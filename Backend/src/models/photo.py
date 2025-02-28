10eea1a5e7a51677c0479d83328c472442581e83
Xowmandon <71954990+Xowmandon@users.noreply.github.com>
2025-02-27 20:53:00 -0800

    Creation of UserPhotos table, storing the users selected photos and indicating their main profile_picture (back-ref on user Record), validated based on size and format on upload method

---
+++ Backend/src/models/photo.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timezone
+from marshmallow import ValidationError, validates
+from Backend.src.validators import ImageValidator
+from Backend.src.validators import ImageValidator
+
+
+from Backend.src.extensions import db, ma # DB and Marshmallow Instances
+from Backend.src.validators.image_val import ImageValidator # Custom Validators
+
+
+class UserPhoto(db.Model):
+    __tablename__ = 'user_photos' # Define the table name
+    
+    # Supplementary ID for Messages
+    id = db.Column(db.Integer, primary_key=True)
+    
+    url = db.Column(db.String, nullable=False, unique=True)  # unique constraint
+    
+    # Foreign Keys - User ID's  of Owner
+    user_id = db.Column(db.String(64), db.ForeignKey('users.id'), nullable=False)
+    is_main_photo = db.Column(db.Boolean, nullable=False)
+
+    # TODO: Implement Date, Time, and TimeZone for Messages in the TimeDate Models
+    upload_date = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
+    size_mb = db.Column(db.Integer, nullable=True)
+    
+    # Unique Constraint on url
+    
+
+# Marshmallow Base Schema for the Message
+class UserPhotoSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = UserPhoto
+        load_instance = True
+        include_relationships = True
+
+
+    # Validation is Done Prior to  Upload to S3 and DB - In Services
+    
+    
