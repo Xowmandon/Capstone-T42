a1537fb1dad20942b365e967a3cba99ece3eb657
Xowmandon <71954990+Xowmandon@users.noreply.github.com>
2025-02-27 21:03:00 -0800

    Addition of Detailed Module Docstring with Addition of s3 and Redis instances

--- Backend/src/extensions.py
+++ Backend/src/extensions.py
@@ -1,23 +1,46 @@
+"""
+
+Flask Extensions Module
+Author: Joshua Ferguson
+
+
+This module initializes and provides extensions for the Flask application.
+
+Extensions:
+- db: SQLAlchemy instance for database management.
+- ma: Marshmallow instance for object serialization/deserialization.
+- flask_jwt: JWTManager for handling JSON Web Tokens.
+- bcrypt: Bcrypt instance for password hashing.
+- redis_client: Redis client for caching and data storage.
+- s3: Boto3 S3 resource for interacting with Amazon S3.
+
+"""
+
 from flask import app
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from flask_socketio import SocketIO
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
+import redis
+import boto3
 
-db = SQLAlchemy()
-ma = Marshmallow(app)
+# ------Flask Extensions (Init with/without App Instance)-----
 
-flask_jwt = JWTManager()
-bcrypt = Bcrypt()
-
-
-# Initialize SocketIO TODO
-#socketio = SocketIO(app, cors_allowed_origins="*")
-
-URL = "https://cowbird-expert-exactly.ngrok-free.app"
+db = SQLAlchemy() # Postgres Client
+ma = Marshmallow(app) # JSON Serialization/Deserilization of Models
+flask_jwt = JWTManager() # JwtManager for Protected Resources
+bcrypt = Bcrypt() # Generating Password Hashes
 
 
+# -----External Services-----
 
+# Redis for Swipe Pool and Caching Frequently Reads
+redis_client = redis.Redis(
+    host='localhost',  # TODO use environment variables
+    port=6379,
+    decode_responses=True
+)
 
-#------------------------------------------------------------
+# S3 Bucket Service for Media/Photo Storage & Retrieval
+s3 = boto3.resource('s3') 
+
