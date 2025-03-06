d59da820c1ca318db6face81688d0d4eb7200c77
Xowmandon <71954990+Xowmandon@users.noreply.github.com>
2025-03-05 22:39:51 -0800

    Complete upload_profile_picture and get_profile_pictures Routes
    - Used to upload  and Retrieve Primary and Gallery profile picture

--- Backend/src/routes/user_routes.py
+++ Backend/src/routes/user_routes.py
@@ -5,10 +5,11 @@ from sqlalchemy.exc import SQLAlchemyError
 from marshmallow import ValidationError
 import logging
 
+from Backend.src import services
 from Backend.src.extensions import db # Import the DB Instance
 import Backend.src.models as models
 from Backend.src.routes.route_helpers import validate_required_params # Import the Models and Schemas
-
+from Backend.src.extensions import media_storage_service, AWS_MEDIA_MAIN_PHOTO_FOLDER, AWS_MEDIA_USER_PHOTO_FOLDER
 
 user_bp = Blueprint('user_bp', __name__)
 
@@ -22,9 +23,7 @@ user_schema = models.user.UserSchema()
 # Admin Protected Route
 
 
-# Get user by Email from DB
-# GET /Users
-# TODO: Test this Route
+
 @user_bp.route('/users/', methods=['GET'])
 @jwt_required()
 def get_user():
@@ -36,7 +35,6 @@ def get_user():
         
     Returns:
         JSON: The user object.
-        
     """
     
     user_id = get_jwt_identity()
@@ -49,7 +47,7 @@ def get_user():
     
     return jsonify(user_schema.dump(user)), 200
 
-@user_bp.route('/users/init/preferences', methods=['POST'])
+@user_bp.route('/users/preferences', methods=['POST'])
 def init_preferences():
     """
     Summary: Initialize a User's Dating Preferences.
@@ -99,7 +97,7 @@ def init_preferences():
     return jsonify({"success": "User Dating Preferences Initialized."}), 201
 
 # Init User with Profile Creation Data
-@user_bp.route('/users/init/profile', methods=['POST'])
+@user_bp.route('/users/profile', methods=['POST'])
 @jwt_required()
 def init_profile():
     """
@@ -140,6 +138,50 @@ def init_profile():
     except Exception as e:
         return jsonify({f"error": "Invalid Data Provided - {e}"}), 400
     
+@user_bp.route('/users/profile', methods=['GET'])
+@jwt_required()
+def get_profile():
+    """
+    Summary: Get the Basic Profile of a User (For Displaying on the Frontend).
+    
+    Header Parameters:
+        X-Authorization: Bearer
+        
+    Return Payload: 
+    JSON object of Basic User Profile Information:
+        - age: int
+        - name: str
+        - gender: str
+        - state: str
+        - city: str
+        - bio: str
+    """
+    user_id = get_jwt_identity()
+    if user_id is None:
+        return jsonify({"error": "Invalid JWT Token."}), 400
+    user = models.user.User.query.get(user_id)
+    if user is None:
+        return jsonify({"error": "User not found."}), 404
+
+    #Check if the User has a Profile 
+    # TODO #7 - Validate Profile Data (Age, Name, gender, etc) Exists with Schema
+    if user.age is None or user.name is None or user.gender is None:
+        return jsonify({"error": "User Profile Not Initialized."}), 400
+
+    # Return the User Profile Information, Age, Gender, State, City, Bio
+    # TODO #6 - Implement User Schema for Profile Information (Age, Name, Gender, State, City, Bio)
+    user_profile = {
+        "age": user.age,
+        "name": user.name,
+        "gender": user.gender,
+        "state": user.state_code,
+        "city": user.city,
+        "bio": user.bio
+    }
+    
+    # Return Schema when Implemented from #6
+    return jsonify(user_profile), 200
+        
 
 @user_bp.route('/users/profile_picture', methods=['POST'])
 @jwt_required()
@@ -150,26 +192,87 @@ def upload_profile_picture():
     Header Parameters:
         X-Authorization: Bearer <JWT Token>
         
-    Sent Payload:
+    Send Payload:
         - profile_picture: BLOB, required
         - is_main_photo: bool, optional, default=False
     
-    Return Payload:
-        JSON: 
-        - A message indicating the success or failure of the profile picture upload.
-        - S3 Stored URL of the Profile Picture    
+    Return JSON Payload:
+        - "success": Profile Picture Uploaded
+        - "url": URL of the Profile Picture in S3
+        
     """
+    user_id = get_jwt_identity()
+    if user_id is None:
+        return jsonify({"error": "Invalid JWT Token."}), 400
+    user = db.session.query(models.user.User).get(user_id)
+    if user is None:
+        return jsonify({"error": "User not found."}), 404
     
-    pass
+    # Retrieve the Profile Picture from the Request, Convert to BLOB
+    profile_picture = request.files.get('profile_picture',type=bytes)
+    if profile_picture is None:
+        return jsonify({"error": "No Profile Picture Provided."}), 400
+    
+    # Create Folder URL for the Profile Picture
+    is_main_photo = request.json.get('is_main_photo', False)
+    if is_main_photo:
+        folder = AWS_MEDIA_MAIN_PHOTO_FOLDER
+    else:
+        folder = AWS_MEDIA_USER_PHOTO_FOLDER
+    
+    # Upload the Profile Picture to S3 with Media Storage Service
+    url = media_storage_service.upload_file(profile_picture, user_id, folder)
+    if url is None:
+        return jsonify({"error": "Profile Picture S3 Upload Failed."}), 500
+    
+    return jsonify({"success": "Profile Uploaded", "url": url}), 201
+    
 
+@user_bp.route('/users/profile_picture', methods=['GET'])
+@jwt_required()
 def get_profile_pictures():
     """
-    Summary: Get the Profile Picture for a User.
+    Summary: Get the Profile Pictures for a User.
     
     Header Parameters:
         X-Authorization: Bearer
+        
+    Return Payload:
+        JSON: 
+        - "main_photo": Main Profile Picture
+        - "user_photos": Additional Profile Pictures
     """
-    pass    
+    user_id = get_jwt_identity()
+    if user_id is None:
+        return jsonify({"error": "Invalid JWT Token}"}), 400
+    user = db.session.query(models.user.User).get(user_id)
+    if user is None:
+        return jsonify({"error": "User not found."}), 404
+    
+    try:
+        # Main Profile Picture
+        main_photo = db.session.query( 
+            models.userPhoto.UserPhoto) \
+        .filter_by(user_id=user_id, is_main_photo=True) \
+        .first()
+        
+        # Additional Profile Pictures
+        additional_photos = db.session.query( 
+            models.userPhoto.UserPhoto) \
+        .filter_by(user_id=user_id, is_main_photo=False) \
+        .all()
+
+    except db.exc.SQLAlchemyError as e: # Database Errors
+        db.session.rollback()
+        return jsonify({f"error": "Database Error - {e}"}), 500
+    
+    # Success Response
+    return jsonify({
+        "main_photo": main_photo,
+        "user_photos": additional_photos
+    }), 200
+
+   
 
 
 # UPDATE User by Email
