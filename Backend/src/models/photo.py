3aeed2ce4e811da26018cabd86bd93b32f3d98f0
Xowmandon <71954990+Xowmandon@users.noreply.github.com>
2025-04-25 11:16:15 -0700

    Enhance UserPhoto model with title and description fields for gallery photos, and add unique constraint on url, is_main_photo, and user_id.

--- Backend/src/models/photo.py
+++ Backend/src/models/photo.py
@@ -13,17 +13,25 @@ class UserPhoto(db.Model):
     # Supplementary ID for Messages
     id = db.Column(db.Integer, primary_key=True)
     
-    url = db.Column(db.String, nullable=False, unique=True)  # unique constraint
-    
     # Foreign Keys - User ID's  of Owner
     user_id = db.Column(db.String(64), db.ForeignKey('users.id'),name="fk_user_photo_owner", nullable=True)
-    is_main_photo = db.Column(db.Boolean, nullable=False,default=False)
 
-    # TODO: Implement Date, Time, and TimeZone for Messages in the TimeDate Models
-    upload_date = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
-    size_mb = db.Column(db.Integer, nullable=True)
+ 
+    url = db.Column(db.String, nullable=False)  # unique constraint
     
-    # Unique Constraint on url
+    # If the Photo is a Gallery Photo, title and  Description Are Needed
+    title = db.Column(db.String(100), nullable=True) #
+    description = db.Column(db.String(500), nullable=True) 
+    
+    # Metadata Fields
+    is_main_photo = db.Column(db.Boolean, nullable=False,default=False)
+    upload_date = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
+    #is_fake = db.Column(db.Boolean, nullable=True) # Is the Photo Fake or Not
+    
+    # Unique Constraint on url and is_main_photo, and user_id
+    db.UniqueConstraint('url', 'is_main_photo', 'user_id')
+    
+    
     
 
 # Marshmallow Base Schema for the Message
